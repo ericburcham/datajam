758817f248642b8f3e13c2adaaf98d702635b615
Eric Burcham <jameseburcham@gmail.com>
2023-02-03 02:08:30 +0000

    add a first pair of tests for each project for use checking CI builds.

---
+++ src/tests/DataJam.UnitTests/AlwaysPassingTests.cs
@@ -0,0 +1,34 @@
+using FluentAssertions;
+
+namespace DataJam.UnitTests;
+
+[TestFixture]
+public class AlwaysPassingTests
+{
+    private int _one;
+    private int _two;
+
+    [OneTimeSetUp]
+    public void OneTimeSetUp()
+    {
+        _one = 1;
+    }
+
+    [SetUp]
+    public void SetUp()
+    {
+        _two = 2;
+    }
+
+    [Test]
+    public void OneShouldBeOne()
+    {
+        _one.Should().Be(1);
+    }
+
+    [Test]
+    public void TwoShouldBeTwo()
+    {
+        _two.Should().Be(2);
+    }
+}
