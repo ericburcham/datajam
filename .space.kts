7bc4006e12522683de4765a8bde9fb87b6b70efa
Eric Burcham <eburcham@eprod.com>
2023-03-03 12:30:09 -0600

    roll back to a sane starting point

--- .space.kts
+++ .space.kts
@@ -12,6 +12,7 @@ val dotNetInstallScript = """
     sudo dpkg -i packages-microsoft-prod.deb
     rm packages-microsoft-prod.deb
     apt-get update
+    apt-get install -y dotnet-sdk-3.1 dotnet-sdk-6.0
 """.trimIndent()
 
 job("Continuous integration build") {
