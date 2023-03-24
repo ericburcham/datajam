1137cb938b6e859fb8545896203f887d9d24042d
Eric Burch <jameseburcham@gmail.com>
2023-03-24 01:21:16 -0500

    Fixed issues with nested Directory.Build.props files.

--- src/build/DataJam.Build/Build.cs
+++ src/build/DataJam.Build/Build.cs
@@ -1,11 +1,3 @@
-// <copyright file="Build.cs" company="Enterprise Products Partners L.P. (Enterprise)">
-// � Copyright 2012 - 2019, Enterprise Products Partners L.P. (Enterprise), All Rights Reserved.
-// Permission to use, copy, modify, or distribute this software source code, binaries or
-// related documentation, is strictly prohibited, without written consent from Enterprise.
-// For inquiries about the software, contact Enterprise: Enterprise Products Company Law
-// Department, 1100 Louisiana, 10th Floor, Houston, Texas 77002, phone 713-381-6500.
-// </copyright>
-
 using Nuke.Common;
 using Nuke.Common.Execution;
 using Nuke.Common.IO;
@@ -16,85 +8,67 @@ using Nuke.Common.Utilities.Collections;
 using static Nuke.Common.IO.FileSystemTasks;
 using static Nuke.Common.Tools.DotNet.DotNetTasks;
 
-/// <summary>
-/// Exposes and coordinates <see cref="Target"/>s for use by Nuke Build.
-/// </summary>
+/// <summary>Exposes and coordinates <see cref="Target" />s for use by Nuke Build.</summary>
 [UnsetVisualStudioEnvironmentVariables]
 public class Build : NukeBuild
 {
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
-    private readonly Configuration _configuration = IsLocalBuild
-        ? Configuration.Debug
-        : Configuration.Release;
+    private readonly Configuration _configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
 
     [Solution]
-    readonly Solution Solution;
+    private readonly Solution _solution = null!;
 
-    AbsolutePath ArtifactsDirectory => RootDirectory / "artifacts";
+    private AbsolutePath ArtifactsDirectory => RootDirectory / "artifacts";
 
-    Target Clean =>
-        targetDefinition => targetDefinition
-             .Executes(() =>
-             {
-                 SourceDirectory.GlobDirectories("**/bin", "**/obj").ForEach(DeleteDirectory);
-                 TestsDirectory.GlobDirectories("**/bin", "**/obj").ForEach(DeleteDirectory);
-                 EnsureCleanDirectory(ArtifactsDirectory);
-             });
-
-    Target Compile =>
-        targetDefinition => targetDefinition
-             .DependsOn(Restore)
-             .Executes(() =>
-             {
-                 DotNetBuild(buildSettings => buildSettings
-                                  .SetProjectFile(Solution)
-                                  .SetConfiguration(_configuration)
-                                  .EnableNoRestore());
-             });
-
-    Target Pack =>
-        targetDefinition => targetDefinition
-             .DependsOn(Test)
-             .Executes(() =>
-             {
-                 DotNetPack(packSettings => packSettings
-                                 .SetProject(Solution)
-                                 .SetOutputDirectory(ArtifactsDirectory)
-                                 .SetIncludeSymbols(true)
-                                 .SetConfiguration(_configuration)
-                                 .EnableNoRestore()
-                                 .EnableNoBuild());
-             });
-
-    Target Restore =>
-        targetDefinition => targetDefinition
-            .DependsOn(Clean)
-            .Executes(() =>
+    private Target Clean =>
+        targetDefinition => targetDefinition.Executes(
+            () =>
             {
-                DotNetRestore(restoreSettings => restoreSettings
-                    .SetProjectFile(Solution));
+                SourceDirectory.GlobDirectories("**/bin", "**/obj").ForEach(DeleteDirectory);
+                TestsDirectory.GlobDirectories("**/bin", "**/obj").ForEach(DeleteDirectory);
+                EnsureCleanDirectory(ArtifactsDirectory);
             });
 
-    AbsolutePath SourceDirectory => RootDirectory / "src" / "code";
+    private Target Compile =>
+        targetDefinition => targetDefinition.DependsOn(Restore)
+                                            .Executes(
+                                                 () =>
+                                                 {
+                                                     DotNetBuild(buildSettings => buildSettings.SetProjectFile(_solution).SetConfiguration(_configuration).EnableNoRestore());
+                                                 });
 
-    Target Test =>
-        targetDefinition => targetDefinition
-             .DependsOn(Compile)
-             .Executes(() =>
-             {
-                 DotNetTest(testSettings => testSettings
-                                 .SetProjectFile(Solution)
-                                 .SetConfiguration(_configuration)
-                                 .EnableNoRestore()
-                                 .EnableNoBuild());
-             });
+    private Target Pack =>
+        targetDefinition => targetDefinition.DependsOn(Test)
+                                            .Executes(
+                                                 () =>
+                                                 {
+                                                     DotNetPack(packSettings => packSettings.SetProject(_solution).SetOutputDirectory(ArtifactsDirectory).SetIncludeSymbols(true).SetConfiguration(_configuration).EnableNoRestore().EnableNoBuild());
+                                                 });
 
-    AbsolutePath TestsDirectory => RootDirectory / "src" / "tests";
+    private Target Restore =>
+        targetDefinition => targetDefinition.DependsOn(Clean)
+                                            .Executes(
+                                                 () =>
+                                                 {
+                                                     DotNetRestore(restoreSettings => restoreSettings.SetProjectFile(_solution));
+                                                 });
 
-    /// Support plugins are available for:
-    /// - JetBrains ReSharper        https://nuke.build/resharper
-    /// - JetBrains Rider            https://nuke.build/rider
-    /// - Microsoft VisualStudio     https://nuke.build/visualstudio
-    /// - Microsoft VSCode           https://nuke.build/vscode
+    private AbsolutePath SourceDirectory => RootDirectory / "src" / "code";
+
+    private Target Test =>
+        targetDefinition => targetDefinition.DependsOn(Compile)
+                                            .Executes(
+                                                 () =>
+                                                 {
+                                                     DotNetTest(testSettings => testSettings.SetProjectFile(_solution).SetConfiguration(_configuration).EnableNoRestore().EnableNoBuild());
+                                                 });
+
+    private AbsolutePath TestsDirectory => RootDirectory / "src" / "tests";
+
+    // Support plugins are available for:
+    // - JetBrains ReSharper        https://nuke.build/resharper
+    // - JetBrains Rider            https://nuke.build/rider
+    // - Microsoft VisualStudio     https://nuke.build/visualstudio
+    // - Microsoft VSCode           https://nuke.build/vscode
     public static int Main() => Execute<Build>(build => build.Pack);
 }
