6c5655af01a8e754ee10275681102e35824c44d1
Eric Burcham <eburcham@eprod.com>
2023-04-21 14:36:01 -0500

    Update .space.kts with better example garbage.

--- .space.kts
+++ .space.kts
@@ -4,32 +4,38 @@
 * For more info, see https://www.jetbrains.com/help/space/automation.html
 */
 
-val dotNetInstallScript = """
+val buildContainerImage = "ubuntu:22.04"
+val buildScript = """
     apt-get update && apt-get install -y apt-utils apt-transport-https
     apt-get install -y curl unzip wget software-properties-common git
-    
-    wget https://packages.microsoft.com/config/ubuntu/22.10/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
-    sudo dpkg -i packages-microsoft-prod.deb
-    rm packages-microsoft-prod.deb
-    apt-get update
-    apt-get install -y dotnet-sdk-3.1 dotnet-sdk-6.0
+    wget https://dot.net/v1/dotnet-install.sh
+    chmod +x ./dotnet-install.sh
+    ./dotnet-install.sh --channel 6.0
+    ./dotnet-install.sh --channel 7.0
+    PATH=${'$'}PATH:${'$'}HOME/.dotnet:${'$'}HOME/.dotnet/tools
+    dotnet --list-sdks
+    ./build.sh
 """.trimIndent()
 
 job("Continuous integration build") {
     startOn {
-        gitPush { enabled = true }
+        gitPush {
+            enabled = true
+
+            branchFilter {
+                -"refs\heads\feature\*"
+            }
+        }
     }
     
-    container("mcr.microsoft.com/dotnet/sdk:6.0-focal") {
+    container(buildContainerImage) {
         resources {
             cpu = 2.cpu
-            memory = 2.gb
+            memory = 4.gb
         }
         
         shellScript {
-            content = dotNetInstallScript + """            
-            	./build.sh
-            """.trimIndent()
+            content = buildScript
         }
     }
 }
