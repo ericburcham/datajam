f74655d5e9042037d0c5ee464d38261322c44556
Eric Burch <jameseburcham@gmail.com>
2023-06-29 23:11:46 -0500

    final cleanup for tonight

--- src/code/DataJam.EntityFrameworkCore/Domains/IEntityFrameworkCoreDomain.cs
+++ src/code/DataJam.EntityFrameworkCore/Domains/IEntityFrameworkCoreDomain.cs
@@ -5,11 +5,3 @@ using Microsoft.EntityFrameworkCore;
 public interface IEntityFrameworkCoreDomain : IDomain<ModelBuilder, DbContextOptions>
 {
 }
-
-public class EntityFrameworkCoreDomain : Domain<ModelBuilder, DbContextOptions>, IEntityFrameworkCoreDomain
-{
-    public EntityFrameworkCoreDomain(IConfigureDomainMappings<ModelBuilder> mappingConfigurator, DbContextOptions configurationOptions)
-        : base(mappingConfigurator, configurationOptions)
-    {
-    }
-}
