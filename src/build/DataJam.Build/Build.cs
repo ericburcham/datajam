a6a76cfae1947aaed42def9745ef48877f707735
Eric Burch <jameseburcham@gmail.com>
2023-07-06 00:47:29 -0500

    working on publish target

--- src/build/DataJam.Build/Build.cs
+++ src/build/DataJam.Build/Build.cs
@@ -2,10 +2,13 @@
 
 using Nuke.Common;
 using Nuke.Common.Execution;
+using Nuke.Common.Git;
 using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
 using Nuke.Common.Tools.DotNet;
 using Nuke.Common.Utilities.Collections;
+
+using Serilog;
 
 using static Nuke.Common.IO.FileSystemTasks;
 using static Nuke.Common.Tools.DotNet.DotNetTasks;
@@ -19,6 +22,9 @@ class Build : NukeBuild
 
     [Solution]
     readonly Solution Solution = null!;
+
+    [GitRepository]
+    readonly GitRepository Repository = null!;
 
     /// <summary>Gets the absolute path for nuget package output.</summary>
     static AbsolutePath PackagesDirectory => RootDirectory / "packages";
@@ -74,7 +80,7 @@ class Build : NukeBuild
     Target CleanRelease => targetDefinition => targetDefinition.Executes(StandardClean(Configuration.Release));
 
     /// <summary>Gets a target that runs the default build.</summary>
-    Target Default => targetDefinition => targetDefinition.DependsOn(CleanDebug, CleanRelease, Restore, BuildDebug, BuildRelease, TestDebug, TestRelease, Pack);
+    Target Default => targetDefinition => targetDefinition.DependsOn(CleanDebug, CleanRelease, Restore, BuildDebug, BuildRelease, TestDebug, TestRelease, Pack, Publish);
 
     /// <summary>Gets a target that runs DotNet Pack for the release configuration.</summary>
     Target Pack =>
@@ -83,6 +89,24 @@ class Build : NukeBuild
             {
                 DotNetPack(packSettings => packSettings.SetProject(Solution).SetOutputDirectory(PackagesDirectory).EnableIncludeSymbols().SetConfiguration(Configuration.Release).EnableNoRestore().EnableNoBuild());
             });
+
+    Target Publish =>
+        targetDefinition => targetDefinition.Requires(() => Repository.IsOnMainOrMasterBranch())
+                                            .Executes(
+                                                 () =>
+                                                 {
+                                                     Log.Information("Commit = {Value}", Repository.Commit);
+                                                     Log.Information("Branch = {Value}", Repository.Branch);
+                                                     Log.Information("Tags = {Value}", Repository.Tags);
+
+                                                     Log.Information("main branch = {Value}", Repository.IsOnMainBranch());
+                                                     Log.Information("main/master branch = {Value}", Repository.IsOnMainOrMasterBranch());
+                                                     Log.Information("release/* branch = {Value}", Repository.IsOnReleaseBranch());
+                                                     Log.Information("hotfix/* branch = {Value}", Repository.IsOnHotfixBranch());
+
+                                                     Log.Information("Https URL = {Value}", Repository.HttpsUrl);
+                                                     Log.Information("SSH URL = {Value}", Repository.SshUrl);
+                                                 });
 
     /// <summary>Gets a target that restores package dependencies.</summary>
     Target Restore => targetDefinition => targetDefinition.After(CleanAll, CleanDebug, CleanOutput, CleanPackages, CleanRelease).Executes(StandardRestore());
