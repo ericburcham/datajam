1ad213e2a4ec1d7d5e36bbd7661c10d6760bb5d2
Eric Burcham <eburcham@eprod.com>
2023-07-06 19:27:39 -0500

    working on XML doc formatting

--- src/code/DataJam/Commands/ICommand.cs
+++ src/code/DataJam/Commands/ICommand.cs
@@ -1,6 +1,8 @@
 namespace DataJam;
 
-/// <summary>Exposes an interface for commands that return no value, or where the value is not used.</summary>
+/// <summary>
+///     Exposes an interface for commands that return no value, or where the value is not used.
+/// </summary>
 public interface ICommand
 {
     /// <summary>Executes the command using the provided data context.</summary>
