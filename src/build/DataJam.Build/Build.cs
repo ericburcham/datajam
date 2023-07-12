3bf38272b66edb1c027bc36692581f26320b793b
Eric Burch <jameseburcham@gmail.com>
2023-07-11 19:40:18 -0500

    Remove all 'WithDependencies' tasks

--- src/build/DataJam.Build/Build.cs
+++ src/build/DataJam.Build/Build.cs
@@ -40,26 +40,17 @@ class Build : NukeBuild
     /// <summary>Gets a target that runs DotNet Build for all build configurations.</summary>
     Target BuildAll => targetDefinition => targetDefinition.DependsOn(BuildDebug, BuildRelease);
 
-    /// <summary>Gets a target that runs DotNet Build for all build configurations after running all dependencies.</summary>
-    Target BuildAllWithDependencies => targetDefinition => targetDefinition.DependsOn(CleanDebug, CleanRelease, Restore, BuildDebug, BuildRelease);
-
     /// <summary>Gets a target that runs DotNet Build for the debug configuration.</summary>
-    Target BuildDebug => targetDefinition => targetDefinition.After(CleanDebug, Restore).Executes(StandardBuild(Configuration.Debug));
-
-    /// <summary>Gets a target that runs DotNet Build for the debug configuration after running all dependencies.</summary>
-    Target BuildDebugWithDependencies => targetDefinition => targetDefinition.DependsOn(CleanDebug, Restore, BuildDebug);
+    Target BuildDebug => targetDefinition => targetDefinition.After(CleanDebug, Restore).Executes(RunBuild(Configuration.Debug));
 
     /// <summary>Gets a target that runs DotNet Build for the Release configuration.</summary>
-    Target BuildRelease => targetDefinition => targetDefinition.After(CleanRelease, Restore).Executes(StandardBuild(Configuration.Release));
-
-    /// <summary>Gets a target that runs DotNet Build for the Release configuration after running all dependencies.</summary>
-    Target BuildReleaseWithDependencies => targetDefinition => targetDefinition.DependsOn(CleanRelease, Restore, BuildRelease);
+    Target BuildRelease => targetDefinition => targetDefinition.After(CleanRelease, Restore).Executes(RunBuild(Configuration.Release));
 
     /// <summary>Gets a target that runs DotNet Clean for all configurations, removes all bin and object folders, and ensures the package output folder is clean.</summary>
     Target CleanAll => targetDefinition => targetDefinition.DependsOn(CleanDebug, CleanRelease, CleanOutput, CleanPackages);
 
     /// <summary>Gets a target that runs DotNet Clean for the debug configuration.</summary>
-    Target CleanDebug => targetDefinition => targetDefinition.Executes(StandardClean(Configuration.Debug));
+    Target CleanDebug => targetDefinition => targetDefinition.Executes(RunClean(Configuration.Debug));
 
     /// <summary>Gets a target that removes all bin and object folders.</summary>
     Target CleanOutput =>
@@ -79,7 +70,7 @@ class Build : NukeBuild
             });
 
     /// <summary>Gets a target that runs DotNet Clean for the release configuration.</summary>
-    Target CleanRelease => targetDefinition => targetDefinition.Executes(StandardClean(Configuration.Release));
+    Target CleanRelease => targetDefinition => targetDefinition.Executes(RunClean(Configuration.Release));
 
     /// <summary>Gets a target that runs the default build.</summary>
     Target Default => targetDefinition => targetDefinition.DependsOn(CleanDebug, CleanRelease, Restore, BuildDebug, BuildRelease, TestDebug, TestRelease, Pack, Publish);
@@ -116,25 +107,16 @@ class Build : NukeBuild
                                                  });
 
     /// <summary>Gets a target that restores package dependencies.</summary>
-    Target Restore => targetDefinition => targetDefinition.After(CleanAll, CleanDebug, CleanOutput, CleanPackages, CleanRelease).Executes(StandardRestore());
+    Target Restore => targetDefinition => targetDefinition.After(CleanAll, CleanDebug, CleanOutput, CleanPackages, CleanRelease).Executes(RunRestore());
 
     /// <summary>Gets a target that runs DotNet Test for all build configurations.</summary>
     Target TestAll => targetDefinition => targetDefinition.DependsOn(TestDebug, TestRelease);
 
-    /// <summary>Gets a target that runs DotNet Test for all build configurations after running all dependencies.</summary>
-    Target TestAllWithDependencies => targetDefinition => targetDefinition.DependsOn(CleanDebug, CleanRelease, Restore, BuildDebug, BuildRelease, TestDebug, TestRelease);
-
     /// <summary>Gets a target that runs DotNet Test for the debug configuration.</summary>
-    Target TestDebug => targetDefinition => targetDefinition.After(CleanDebug, Restore, BuildDebug).Executes(StandardTest(Configuration.Debug));
-
-    /// <summary>Gets a target that runs DotNet Test for the debug configuration after running all dependencies.</summary>
-    Target TestDebugWithDependencies => targetDefinition => targetDefinition.DependsOn(CleanDebug, Restore, BuildDebug, TestDebug);
+    Target TestDebug => targetDefinition => targetDefinition.After(CleanDebug, Restore, BuildDebug).Executes(RunTests(Configuration.Debug));
 
     /// <summary>Gets a target that runs DotNet Test for the release configuration.</summary>
-    Target TestRelease => targetDefinition => targetDefinition.After(CleanRelease, Restore, BuildRelease).Executes(StandardTest(Configuration.Release));
-
-    /// <summary>Gets a target that runs DotNet test for the release configuration after running all dependencies.</summary>
-    Target TestReleaseWithDependencies => targetDefinition => targetDefinition.DependsOn(CleanRelease, Restore, BuildRelease, TestRelease);
+    Target TestRelease => targetDefinition => targetDefinition.After(CleanRelease, Restore, BuildRelease).Executes(RunTests(Configuration.Release));
 
     // Support plugins are available for:
     // - JetBrains ReSharper        https://nuke.build/resharper
@@ -146,7 +128,7 @@ class Build : NukeBuild
     /// <summary>Creates an action that performs the project's standard DotNet Build task.</summary>
     /// <param name="configuration">The build configuration to use.</param>
     /// <returns>An action that performs the project's standard DotNet Build task.</returns>
-    Action StandardBuild(Configuration configuration) =>
+    Action RunBuild(Configuration configuration) =>
         () =>
         {
             DotNetBuild(buildSettings => buildSettings.SetProjectFile(Solution).SetConfiguration(configuration).EnableNoRestore());
@@ -155,7 +137,7 @@ class Build : NukeBuild
     /// <summary>Creates an action that performs the project's standard DotNet Clean task.</summary>
     /// <param name="configuration">The build configuration to use.</param>
     /// <returns>An action that performs the project's standard DotNet Clean task.</returns>
-    Action StandardClean(Configuration configuration) =>
+    Action RunClean(Configuration configuration) =>
         () =>
         {
             DotNetClean(settings => settings.SetConfiguration(configuration).SetProject(Solution));
@@ -163,13 +145,13 @@ class Build : NukeBuild
 
     /// <summary>Creates an action that performs the project's standard DotNet Restore task.</summary>
     /// <returns>An action that performs the project's standard DotNet Restore task.</returns>
-    Action StandardRestore() =>
+    Action RunRestore() =>
         () =>
         {
             DotNetRestore(restoreSettings => restoreSettings.SetProjectFile(Solution));
         };
 
-    Action StandardTest(Configuration configuration) =>
+    Action RunTests(Configuration configuration) =>
         () =>
         {
             DotNetTest(testSettings => testSettings.SetProjectFile(Solution).SetConfiguration(configuration).EnableNoRestore().EnableNoBuild().EnableNoRestore());
