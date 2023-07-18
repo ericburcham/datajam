6ff84b90cd208b544efbef26f9c5f438e70c5cc0
Eric Burcham <eburcham@eprod.com>
2023-07-18 17:10:29 -0500

    try to register nuget feed

--- .space.kts
+++ .space.kts
@@ -9,7 +9,7 @@ val buildScript = """
     PATH=${'$'}PATH:${'$'}HOME/.dotnet:${'$'}HOME/.dotnet/tools
     dotnet --list-sdks
     
-    dotnet nuget add source $JB_SPACE_CLIENT_ID -n space -u "%JB_SPACE_CLIENT_ID%" -p "%JB_SPACE_CLIENT_SECRET%" --store-password-in-clear-text
+    dotnet nuget add source $NuGetSpaceTargetUrl -n space -u "%JB_SPACE_CLIENT_ID%" -p "%JB_SPACE_CLIENT_SECRET%" --store-password-in-clear-text
 
     ./build.sh
 """.trimIndent()
