9ac6ce4ed237355499c4e3e5d4566918c31a87ae
Eric Burcham <eburcham@eprod.com>
2024-11-25 21:41:11 -0600

    final code cleanup for the night

--- src/tests/DataJam.EntityFrameworkCore.MsSql.IntegrationTests/TestFixtureConstructorParameterProvider.cs
+++ src/tests/DataJam.EntityFrameworkCore.MsSql.IntegrationTests/TestFixtureConstructorParameterProvider.cs
@@ -4,8 +4,6 @@ using System.Collections;
 
 using Family;
 
-using Microsoft.EntityFrameworkCore;
-
 using TestSupport.EntityFrameworkCore;
 
 public static class TestFixtureConstructorParameterProvider
