7073c18b9eaa6944eea21bdfd9e40b2b8a201c74
Eric Burcham <eburcham@eprod.com>
2025-03-17 16:54:34 -0500

    remove accidentally left test types

--- src/tests/DataJam.EntityFrameworkCore.MsSql.IntegrationTests/TestDependencyProvider.cs
+++ src/tests/DataJam.EntityFrameworkCore.MsSql.IntegrationTests/TestDependencyProvider.cs
@@ -1,8 +1,6 @@
 namespace DataJam.EntityFrameworkCore.MsSql.IntegrationTests;
 
 using System;
-using System.Threading;
-using System.Threading.Tasks;
 
 using TestSupport.Dependencies;
 using TestSupport.Dependencies.TestContainers;
@@ -14,38 +12,7 @@ internal class TestDependencyProvider : CompositeTestDependencyProvider
     private TestDependencyProvider()
     {
         Register(ContainerConstants.MSSQL_CONTAINER_NAME, new DefaultMsSqlTestContainerBuilder());
-        Register(nameof(UselessTestDependency), new UselessTestDependencyBuilder());
     }
 
     public static TestDependencyProvider Instance => _instance.Value;
-}
-
-internal class UselessTestDependency : IStartableTestDependency, IDisposable
-{
-    public string Dependency => string.Empty;
-
-    object ITestDependency.Dependency => Dependency;
-
-    public void Dispose()
-    {
-        Console.WriteLine($"{nameof(UselessTestDependency)} disposed.");
-    }
-
-    public void Start()
-    {
-        Console.WriteLine($"{nameof(UselessTestDependency)} started.");
-    }
-
-    public void Stop()
-    {
-        Console.WriteLine($"{nameof(UselessTestDependency)} stopped.");
-    }
-}
-
-internal class UselessTestDependencyBuilder : IBuildTestDependencies<UselessTestDependency>
-{
-    public UselessTestDependency Build()
-    {
-        return new();
-    }
 }
